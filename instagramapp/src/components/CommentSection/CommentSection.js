5db250fff30989ae123bd3b728f87841fb2c9976
AJLambda <apj2277@gmail.com>
2019-03-25 13:48:58 -0700

    finished component skeleton

---
+++ instagramapp/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import "./CommentSection.css";
+
+export default CommentSection;
---
+++ instagramapp/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import "./PostContainer.css";
+
+export default PostContainer;
